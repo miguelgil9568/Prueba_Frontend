e83e5ac22483e2eb13bc7898b377c9c5419df9d5
magila <magila@indracompany.com>
2026-05-13 19:34:35 -0500

    .button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { RatingModule } from 'primeng/rating';
+import { GalleriaModule } from 'primeng/galleria';
+import { DataViewModule } from 'primeng/dataview';
+import { TagModule } from 'primeng/tag';
+import { DividerModule } from 'primeng/divider';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { TableModule } from 'primeng/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,9 +32,22 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    CardModule,
+    ButtonModule,
+    DropdownModule,
+    RatingModule,
+    GalleriaModule,
+    DataViewModule,
+    TagModule,
+    DividerModule,
+    ToastModule,
+    ToolbarModule,
+    InputNumberModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
